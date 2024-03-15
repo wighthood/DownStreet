e8bea8c90deab7e649094ea1717123661ef4383e
wighthood <zefires@gmail.com>
2024-03-15 09:59:19 +0100

    destroy obstacle once they leave view

--- Assets/Script/MapMove.cs
+++ Assets/Script/MapMove.cs
@@ -6,4 +6,12 @@ public class MapMove : MonoBehaviour
     {
         transform.position += new Vector3(0, 3,0)*Time.deltaTime;
     }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.CompareTag("Destroy"))
+        {
+            Destroy(gameObject);
+        }
+    }
 }
