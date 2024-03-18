e13abfc66878e986518cfd1fdbb88b79b4a0f4b1
wighthood <zefires@gmail.com>
2024-03-18 16:55:52 +0100

    added score system

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -1,18 +1,30 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerControl : MonoBehaviour
 {
    [SerializeField] private Rigidbody2D Rigidbody2D;
+   [SerializeField] private TMP_Text text;
    [SerializeField] private int speed;
+   private int score =0;
    public bool isgrounded = false;
-   public void LeftRight (InputAction.CallbackContext context)
+
+    public void LeftRight(InputAction.CallbackContext context)
     {
         Rigidbody2D.velocity = (context.ReadValue<Vector2>() * speed);
     }
-
     public void shoot(InputAction.CallbackContext context)
     {
         Debug.Log("pew");
+    }
+
+    private void Update()
+    {
+        if (!isgrounded)
+        {
+            score += 100;
+            text.SetText("score : " + score);
+        }
     }
 }
